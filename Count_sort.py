e3cedf1bd2920da6c640eef3a5595fe70e9ddeb5
Shobhit Maheshwari <shobhit20897@gmail.com>
2017-02-12 21:38:56 +0530

    Create Count_sort.py

---
+++ Count_sort.py
@@ -0,0 +1,28 @@
+
+
+def counting_sort(array, maxval):
+    """in-place counting sort"""
+    m = maxval + 1
+    count = [0] * m               # init with zeros
+    for a in array:
+        count[a] += 1          # count occurences
+    i = 0
+    print(count)
+    for a in range(m):            # emit
+        for c in range(count[a]): # - emit 'count[a]' copies of 'a'
+            array[i] = a
+            i += 1
+    return array
+
+def main():
+    num_array = list()
+    num = input("Enter the no. of elements in the array:- ")    # the length of the number list
+    print ('Feed the numbers in array:- ')  # input of the numbers
+    for i in range(int(num)):
+        n = input("number :")
+        num_array.append(int(n))    #input number is appended to the list
+    num_array = counting_sort(num_array,max(num_array))
+    print(num_array)
+
+if __name__ == "__main__":
+    main()
