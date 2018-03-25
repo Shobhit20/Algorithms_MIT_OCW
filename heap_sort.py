d83e62db922fd16c7c503fdb52144b0a82329c66
Shobhit20 <shobhit20897@gmail.com>
2018-03-25 23:29:13 +0530

    Heap sort rectified

--- heap_sort.py
+++ heap_sort.py
@@ -1,49 +1,42 @@
-#--------------------Heap Sort--------------------
-'''The heap sort algorithm is a sorting algorithm having a worst 
-case time complexity of O(nlog(n)). The algorithm is implemented 
-using a data structure called heaps whicha are binary trees with 
-some fixed property '''
+import random
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1     # left = 2*i + 1
+    r = 2 * i + 2     # right = 2*i + 2
+ 
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[i] < arr[l]:
+        largest = l
+ 
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+ 
+    # Change root, if needed
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap
+ 
+        # Heapify the root.
+        heapify(arr, n, largest)
+ 
+# The main function to sort an array of given size
+def heapSort(arr):
+    n = len(arr)
+ 
+    # Build a maxheap.
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+ 
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]   # swap
+        heapify(arr, i, 0)
+ 
+arr = random.sample(range(1, 100),30)
+print ('Current status of the array is:- ',arr)
+heapSort(arr)	# function call to sortlist and storing the ret. value in peak
 
-# The function checks for the left and right sub child in order to build a max heap. A max heap satisfies the property that the child are always smaller than the parent
-def maxheapify(list1,length,i):
-    left_child=2*i+1    # The left child of parent i
-    right_child=2*i+2   # The right child of parent i
-    if left_child<=(length-1) and list1[i]<list1[left_child]:
-        largest=left_child
-    elif right_child<=(length-1) and list1[i]<list1[right_child]:
-        largest=right_child
-    else:
-        largest=i
-    if largest!=i:
-        list1[i],list1[largest]=list1[largest],list1[i]     #swapping the child with value greater than parent with the parent
-        maxheapify(list1,length,largest)
-
-# The function sorts the unsorted array
-def heap_sort(list1,length):
-    for i in range(len(list1)):
-        list1[0],list1[length-1]=list1[length-1],list1[0]   #swapping of the first element(largest) and last element(smallest) of the array
-        length=length-1     # Decreasing the size of list
-        maxheapify(list1,length,0)
-    return list1
-
-# The function calls the maxheapify to build a max heap and then calls the heap_sort to sort the list
-def build_maxheap(list1):
-    length=len(list1)   
-    for i in range((len(list1)-1)//2,-1,-1):
-        maxheapify(list1,length,i)
-    return list(heap_sort(list1,length))
-
-
-def main():
-    num_array = list()
-    num = input("Enter the no. of elements in the array:- ")    # the length of the number list
-    print ('Feed the numbers in array:- ')  # input of the numbers
-    for i in range(int(num)):
-        n = input("number :")
-        num_array.append(int(n))    #input number is appended to the list
-    print ('Current status of the array is:- ',num_array)
-    sorted_list=build_maxheap(num_array)   # function call to build_maxheap and storing the ret. value in sorted_list
-    print("Sorted list is:- ",sorted_list)  # printing the sorted list
-
-if __name__=="__main__":
-    main()
+n = len(arr)
+print ("Sorted array is:- ", arr)
